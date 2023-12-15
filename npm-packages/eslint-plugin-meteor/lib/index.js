9c83016a3ff24205476af6562f0e517a1029ecc3
matheusccastro <camargodecastromatheus@gmail.com>
2023-12-15 14:43:00 -0300

    feat: create rule for `no-meteor-wrap-async-usage`.

--- npm-packages/eslint-plugin-meteor/lib/index.js
+++ npm-packages/eslint-plugin-meteor/lib/index.js
@@ -13,6 +13,7 @@ const allRules = {
   'no-sync-mongo-methods-on-server': require('./rules/no-sync-mongo-methods-on-server/no-sync-mongo-methods-on-server'),
   'no-fibers-future-usage': require('./rules/no-fibers-future-usage'),
   'no-promise-fibers-usage': require('./rules/no-promise-fibers-usage'),
+  'no-meteor-wrap-async-usage': require('./rules/no-meteor-wrap-async-usage'),
 };
 
 module.exports = {
@@ -40,6 +41,7 @@ module.exports = {
         'meteor/no-sync-mongo-methods-on-server': 0,
         'meteor/no-fibers-future-usage': 0,
         'meteor/no-promise-fibers-usage': 0,
+        'meteor/no-meteor-wrap-async-usage': 0,
       },
     },
   },
